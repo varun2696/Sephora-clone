746fe4e7d4fb18b4cb4ad00d191ba239d2d53916
varun <varuvrk02@gmail.com>
2022-12-18 17:38:11 +0530

    All login and register work Completed by Varun

--- loginScripts/login.js
+++ loginScripts/login.js
@@ -1,60 +1,105 @@
 
-// do not touch this *********
-const login_Url = 'https://6398172cfe03352a94c47ae1.mockapi.io/login_user'
+//  *********  do not touch this ***********
 
+const login_Url = 'https://6398172cfe03352a94c47ae1.mockapi.io/login_user'
 
 const registerUrl = 'https://636f9027f2ed5cb047e01947.mockapi.io/reg_mail'
 
-        // **************
+// ************** end **********************
 
 
 
-// this is for pop pup modal
 
-    const LoginName = document.querySelector('#loginName');
-    LoginName.addEventListener('click', showModal)
+// this is for show userName in navbar and signout functionality
 
-    function showModal() {
-        document.querySelector('.loginform').classList.add('showLoginform');
+const userName = sessionStorage.getItem('c4raUser') || "LogIn";
+
+const LoginName = document.querySelector('#loginName');
+
+LoginName.innerHTML = userName;
+
+// LoginName.addEventListener('click', ()=>{
+//     LModal_div.style.display = 'flex'
+// })
+
+const signout = document.getElementById('signout');
+signout.addEventListener('click', () => {
+    if (userName !== "LogIn") {
+        // alert('You are signout from the site')
+        modal_div.style.display = 'flex';
+        alertMessage.innerHTML = 'You are Signout from the site';
+        sessionStorage.removeItem('c4raUser');
+        setTimeout(function () {
+            window.location.reload();
+        }, 3000);
     }
+})
 
-    function closeModal(){
-        document.querySelector('.loginform').classList.remove('showLoginform');
-    }
-     
-    let crossbtn = document.querySelector('#crossbtn');
-    crossbtn.addEventListener('click', closeModal);
-
-
-// ************************
+//  ********** END ********
 
 
 
 
+// ********* POpPup message **********
+
+const modal_div = document.querySelector('.PopPupMessage');
+const alertMessage = document.querySelector('#alertMessage');
+const closeAlert = document.querySelector('.modal-close');
+
+closeAlert.addEventListener('click', event => {
+    modal_div.style.display = 'none';
+    alertMessage.innerHTML = '';
+})
+
+//  *********** end *********
+
+
+
+
+
+// ******** SHOW LOGIN MODAL  latest changes ***********
+
+
+const LoginModal_div = document.querySelector('.loginModal');
+const CloseLoginModal = document.querySelector('#loginCrossbtn');
+
+
+LoginName.addEventListener('click', ()=>{
+    LoginModal_div.style.display = 'flex'
+})
+
+
+CloseLoginModal.addEventListener('click', event => {
+    LoginModal_div.style.display = 'none';
+})
+
+
+// ********* end ************
+
+
+
+// ***** CRUD operations Start *****
 
 const login_btn = document.getElementById('login_btn');
 let LoginData;
 
-
-// showUsername();
-
-// const showName = document.querySelector('h2');
-
-// let userName = sessionStorage.getItem('c4raUser') || "Login"
-// showName.innerHTML = userName;
-
 login_btn.addEventListener('click', () => {
 
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    if(username == '' || password == ''){
-        alert('Please enter the details')
+    let username = document.getElementById('UserName').value;
+    let password = document.getElementById('PassWord').value;
+    if (username == '' || password == '') {
+        // alert('Please enter the details')
+        modal_div.style.display = 'flex';
+        alertMessage.innerHTML = 'Please enter all the details';
+
     }
-    else{
+    else {
         LoginUser(username, password)
     }
-   
+
 })
+
+
 
 const LoginUser = async (username, password) => {
 
@@ -73,11 +118,17 @@ const LoginUser = async (username, password) => {
                     return el;
                 }
             });
-            
-            console.log('LoginDATA',DATA)
-            if(username == DATA[0].username && password == DATA[0].password){
-                alert('login sucees');
+
+            console.log('LoginDATA', DATA)
+            if (username == DATA[0].username && password == DATA[0].password) {
+                // alert('login sucees');
+                modal_div.style.display = 'flex';
+                alertMessage.innerHTML = 'Login Sucess';
                 sessionStorage.setItem('c4raUser', username);
+
+                setTimeout(function () {
+                    window.location.reload();
+                }, 3000);
 
                 let postData = DATA[0]
                 // console.log('LogedDATA', postData)
@@ -87,64 +138,32 @@ const LoginUser = async (username, password) => {
                     Password: postData.password
                 }
                 let loginUserName = LoginData.Username
-                CheckUserIfExists(loginUserName)
+                CheckUserIfAlreagyLogin(loginUserName)
 
-                // window.location.reload();
-                // window.location.href = './Homepage.html'
-            } 
-            else{
-                alert('Please enter valid username and password')
+            }
+            else {
+                // alert('Please enter valid username and password')
+                modal_div.style.display = 'flex';
+                alertMessage.innerHTML = 'Please enter valid username and password';
             }
 
         }
 
     }
     catch (error) {
-        alert('Something went wrong User not found')
+        // alert('Something went wrong User not found')
+        modal_div.style.display = 'flex';
+        alertMessage.innerHTML = 'Please enter valid username and password';
         console.log('error', error)
     }
 }
 
 
-// let signout = document.getElementById('signout');
-// signout.addEventListener('click',  ()=>{
-
-//     alert('You are signout from the site')
-//     sessionStorage.removeItem('c4raUser');
-//     window.location.reload();
-// })
-
-
-async function PostLoginData (LoginData){
-
-    try {
-        let reg_req = await fetch(login_Url, {
-            method: 'POST',
-            body: JSON.stringify(LoginData),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-
-        })
-        // console.log(reg_req)
-        if (reg_req.ok) {
-            let data = await reg_req.json()
-            console.log('PostData', data)
-        }
-
-        // console.log(data)
-    }
-    catch (error) {
-        console.log('er', error)
-    }
-}
 
 
 
+const CheckUserIfAlreagyLogin = async (loginUserName) => {
 
-
-const CheckUserIfExists = async (loginUserName) => {
-    
     try {
         let Check_res = await fetch(login_Url, {
             method: 'GET'
@@ -163,23 +182,55 @@ const CheckUserIfExists = async (loginUserName) => {
             if (DATA.length !== 0) {
                 if (loginUserName == DATA[0].Username) {
                     console.log('User is Already exists in LoginUrl');
-                    // window.location.href = './Homepage.html'
+                    // window.location.reload();
                 }
                 else {
-                    PostLoginData (LoginData)
-                    // window.location.href = './Homepage.html'
+                    PostLoginData(LoginData)
+                    // window.location.reload();
                 }
             }
             else {
-                PostLoginData (LoginData)
-                // window.location.href = './Homepage.html'
+                PostLoginData(LoginData)
+                // window.location.reload();
             }
 
         }
 
     }
     catch (error) {
-        alert('Error while fetching')
+        // alert('Error while fetching')
         console.log('error', error)
     }
 }
+
+
+
+
+async function PostLoginData(LoginData) {
+
+    try {
+        let reg_req = await fetch(login_Url, {
+            method: 'POST',
+            body: JSON.stringify(LoginData),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+
+        })
+        // console.log(reg_req)
+        if (reg_req.ok) {
+            let data = await reg_req.json()
+            // console.log('PostData', data)
+        }
+
+        // console.log(data)
+    }
+    catch (error) {
+        console.log('er', error)
+    }
+}
+
+
+// ***** CRUD operations end *****
+
+
